e325f974857986465d1196895edf3ceba187ad6b
Cristian Castro V <cristian1853@gmail.com>
2017-03-29 10:54:42 -0500

    Add Url scaffold.

---
+++ app/views/urls/_url.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! url, :id, :url, :time, :name, :message, :health_code, :created_at, :updated_at
+json.url url_url(url, format: :json)
---
+++ db/migrate/20170329154913_create_urls.rb
@@ -0,0 +1,13 @@
+class CreateUrls < ActiveRecord::Migration[5.0]
+  def change
+    create_table :urls do |t|
+      t.string :url
+      t.integer :time
+      t.string :name
+      t.string :message
+      t.integer :health_code
+
+      t.timestamps
+    end
+  end
+end
